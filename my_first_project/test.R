b8a6f7c46eaff47f107491fd1474d62b7bb5d536
meseret-assefa <m_kerga@yahoo.com>
2018-05-18 20:16:24 +0200

    xyz
    xyz

--- my_first_project/test.R
+++ my_first_project/test.R
@@ -1,4 +1,27 @@
 
-data("mtcars")
+## load library
+library(tidyverse)
+library(magrittr)
 
-head(mtcars)
+## load data
+data("iris")
+
+## select setosa group
+setosa = iris[iris$Species == "setosa", ]
+
+## Option-2: select setosa
+setosa_2 = iris %>% 
+  dplyr::filter(Species == "setosa") %>% 
+  dplyr::select(c(1,2))
+
+## Ex11. Find setosa flowers whose Sepal.Length2 value are non-missing from IRIS3 dataset
+irisnew=iris3[iris3$Species == "setosa" & !is.na(iris3$Sepal.Length2), ]
+
+## option-2
+irisnew_2 = iris3 %>% 
+  dplyr::filter(Species == "setosa") %>% 
+  dplyr::filter(!is.na(Sepal.Length2))
+
+
+
+
